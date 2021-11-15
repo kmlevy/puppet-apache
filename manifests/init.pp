71ae0ab242b2aac38e7dae1f8e71783c233da284
root <root@mom.globomantics.loc>
2021-11-14 20:25:03 -0600

    added service files

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,10 +7,13 @@
 class apache (
   String $install_name, 
   Array $config_paths, 
+  String $service_name, 
 ) {
   contain apache::install
   contain apache::config
+  contain apache::service
 
   Class['::apache::install']
   -> Class['::apache::config']
+  ~> Class['::apache::service']
 }
